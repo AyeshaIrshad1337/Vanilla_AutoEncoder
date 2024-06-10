b64e43a79417f1adc9235397cdc92e8ff10c3c7f
Ayesha Irshad <ayeshaitshad124@gmail.com>
2024-06-11 01:37:50 +0500

    code completed

--- models/model.py
+++ models/model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Input, Reshape
+
+from tensorflow.keras.models import Model
 def create_model(input_img):
     latent_dim = 64
     # Encoder
@@ -13,3 +14,20 @@ def create_model(input_img):
     decoded = Dense(128, activation='relu')(decoded)
     decoded = Dense(784, activation='sigmoid')(decoded)
     return decoded
+# Define the encoder
+def build_encoder(input_shape):
+    encoder_input = Input(shape=input_shape)
+    x = Flatten()(encoder_input)
+    x = Dense(128, activation='relu')(x)
+    encoder_output = Dense(64, activation='relu')(x)
+    encoder = Model(encoder_input, encoder_output, name="encoder")
+    return encoder
+
+# Define the decoder
+def build_decoder(encoded_shape):
+    decoder_input = Input(shape=encoded_shape)
+    x = Dense(128, activation='relu')(decoder_input)
+    x = Dense(28 * 28, activation='sigmoid')(x)
+    decoder_output = Reshape((28, 28, 1))(x)
+    decoder = Model(decoder_input, decoder_output, name="decoder")
+    return decoder
--- train.py
+++ train.py
@@ -1,17 +1,37 @@
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
-from models.model import create_model
+from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
+from models.model import build_encoder, build_decoder
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
+# reshaping images into channel
+x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
 
-# Flatten the 28x28 images into vectors of size 784
-x_train = x_train.reshape((len(x_train), -1))
-x_test = x_test.reshape((len(x_test), -1))
-input_img = Input(shape=(784,))
-decoder=create_model(input_img)
-autoencoder = Model(input_img, decoder)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+input_shape = (28, 28, 1)
+encoded_shape = 64
+
+encoder = build_encoder(input_shape)
+decoder = build_decoder(encoded_shape)
+
+autoencoder_input = Input(shape=input_shape)
+encoded = encoder(autoencoder_input)
+decoded = decoder(encoded)
+
+autoencoder = Model(autoencoder_input, decoded, name="autoencoder")
+
+# Compile the autoencoder
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the autoencoder
 autoencoder.fit(x_train, x_train, epochs=50, batch_size=256, shuffle=True, validation_data=(x_test, x_test))
 
+# Evaluate the autoencoder
+loss = autoencoder.evaluate(x_test, y_test)
+print(f'Test loss: {loss}')
+
+# Save the encoder and decoder models
+encoder.save("encoder.h5")
+decoder.save("decoder.h5")    
